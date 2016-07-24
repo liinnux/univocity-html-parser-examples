7fa3121b354592f21adc3e442a8c40095e0a7198
Jeronimo Backes <jbax@univocity.com>
2016-07-24 14:55:18 +0930

    Updating license and copyright notice

--- src/test/java/com/univocity/parsers/examples/HtmlParserExamples.java
+++ src/test/java/com/univocity/parsers/examples/HtmlParserExamples.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2013 uniVocity Software Pty Ltd. All rights reserved.
+ * This file is subject to the terms and conditions defined in file
+ * 'LICENSE.txt', which is part of this source code package.
+ */
+
 package com.univocity.parsers.examples;
 
 import com.univocity.api.common.*;
@@ -7,7 +13,6 @@ import com.univocity.parsers.common.*;
 import com.univocity.parsers.common.processor.core.*;
 import com.univocity.parsers.common.record.*;
 import com.univocity.parsers.conversions.*;
-import com.univocity.parsers.html.parser.builder.*;
 import com.univocity.parsers.html.processor.*;
 import org.testng.annotations.*;
 
